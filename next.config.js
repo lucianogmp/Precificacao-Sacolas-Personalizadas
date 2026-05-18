ac029c3edc2472d73a6c10cfbd013813ea46ef0e
lucianogmp <lucianolemos08@gmail.com>
2026-05-18 20:29:34 -0300

    Fix GitHub Pages deploy

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const repo = 'Precificacao-Sacolas-Personalizadas';
+
+module.exports = {
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true,
+  },
+};
