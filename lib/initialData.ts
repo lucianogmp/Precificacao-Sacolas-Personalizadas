ec890a0fe50e2cd487a1b4cb26675559d325e8b5
lucianogmp <lucianolemos08@gmail.com>
2026-05-17 21:38:19 -0300

    Add initial data configuration

---
+++ lib/initialData.ts
@@ -0,0 +1,12 @@
+export const initialData = {
+  depreciacao: {
+    facaP: 0.14,
+    facaM: 0.175,
+    facaG: 0,
+    maquinaCorte: 0.1653,
+    impressora: 1.2785,
+  },
+  custoHora: 10.6883,
+  margemDesejada: 50,
+  horasTrabalhadasMes: 247,
+};
