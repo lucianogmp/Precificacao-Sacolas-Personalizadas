59dc27490922f83f091e6bdd60e4e24c6be9a266
lucianogmp <lucianolemos08@gmail.com>
2026-05-18 21:10:25 -0300

    Remove unused label import

--- components/TabsConfiguracao.tsx
+++ components/TabsConfiguracao.tsx
@@ -7,7 +7,6 @@ import { ConfiguracaoPrecificacao, TamanhoSacola } from '@/lib/types';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 interface Props {
